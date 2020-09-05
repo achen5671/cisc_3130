1a85e17ddd495ae2e33051640d32af424e7c973a
achen5671 <44120813+achen5671@users.noreply.github.com>
2020-09-05 13:02:46 -0400

    lab two [done!!]

--- lab_two/Artist.java
+++ lab_two/Artist.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
 public class Artist{
+	//instance vars
 	private String artist;
 	private int[] trackPosition;
 	private String[] trackName;
 	private int[] numStreamed;
 	private int numTracks = 0;
 
+	//constructor
 	public Artist(String artist, int trackPosition, String trackName, int numStreamed){
 		this.artist = artist; 
 		this.trackPosition = new int[200];
@@ -18,43 +20,30 @@ public class Artist{
 		this.numTracks++;
 	}
 
-	// public Artist(int trackPosition, String trackName, int numStreamed){
-	// 	this.trackPosition[numTracks] = trackPosition;
-	// 	this.trackName[numTracks] = trackName;
-	// 	this.numStreamed[numTracks] = numStreamed;
-	// 	this.numTracks++;
-	// }
-
-
-
 	//Getters
 	public String getArtist(){
 		return this.artist;
 	}
-
-	// public String getNumStreamed(){
-	// 	return Arrays.toString(this.numStreamed);
-	// }
-
-	// public String getTrackPosition(){
-	// 	return Arrays.toString(this.trackPosition);
-	// }
-
-	public String getTrackName(){
-		for(int i =0; i< numTracks; i++){
-			return this.trackName[i];
-		}
-		return "";
-	}
 	
-	public String toString(){
-		return Arrays.toString(trackName);
-	}
-
 	public int getNumTracks(){
 		return numTracks;
 	}
 
+	//toString() prints out info 
+	public String toString(){
+		String s = "";
+		for(int i = 0; i < numTracks;i++){
+			s += trackPosition[i] + ", " + artist + ", " + trackName[i] + ", " + numStreamed[i] + "\n";
+		}
+		return s + "\n";
+	}
+
+	/**
+	**input:**
+	trackposition, trackname, track # numstreamed.
+	**process/output**
+	set info to arrays
+	**/
 	public void addTrack(int trackPosition, String trackName, int numStreamed){
 		this.trackPosition[numTracks] = trackPosition;
 		this.trackName[numTracks] = trackName;
--- lab_two/Main.java
+++ lab_two/Main.java
@@ -6,67 +6,92 @@ public class Main{
 	public static void main(String[] args){
 		//Path name to Spotify csv
 		String path = "C:\\Users\\Andy Chen\\Documents\\GitHub\\cisc_3130\\lab_two\\regional-global-daily-latest.csv";
-		
-		//try catch block. It throws neccesary error message if something goes wrong.
+		//try catch block. Throws neccesary error message if any.
 		try{
-			//Print output to neccesary file
+			//Print output to txt file.
 			PrintWriter outFile = new PrintWriter("C:\\Users\\Andy Chen\\Documents\\GitHub\\cisc_3130\\lab_two\\Artists-WeekOf09.03.2020.txt");
 			//Obtaining input from file
 			BufferedReader br = new BufferedReader(new FileReader(path));
+			//ArrayList of artists
 			ArrayList<Artist> artistTrack = new ArrayList<Artist>();
-			Artist artist;
-			//nullString 
-			String info = "";
-			//READS FIREST LINE (HEADER)
-			br.readLine();
-			//Loop to go through the data
-			while((info = br.readLine()) != null){
-				info = info.replaceAll(", ", " ");
-				info = info.replaceAll("\"", "");
-				
-				String[] tokens = info.split(",");
-				
-				//Find track position
-				int trackPosition = Integer.parseInt(tokens[0]);
-				//Find Song Artist
-				String trackName = tokens[1];
-				//Find track name
-				String artistName = tokens[2];
-				//Find # of time Stream
-				int numStreamed = Integer.parseInt(tokens[3]);
-
-				if(artistTrack.size() == 0){
-					artist = new Artist(artistName, trackPosition, trackName,numStreamed);
-					artistTrack.add(artist);
-				}else{
-					for(int i = 0; i < 1; i++){
-						if(artistName.equals(artistTrack.get(i).getArtist())){
-							artistTrack.get(i).addTrack(trackPosition, trackName,numStreamed);
-						}else{
-							artist = new Artist(artistName,trackPosition, trackName,numStreamed);
-							artistTrack.add(artist);
-						}
-					}
-				}
-
-				
-
-				
-				
-
-			}System.out.println(artistTrack.get(0));
-
 			
+			//READS FIRST LINE (HEADER) and prints to output file
+			outFile.println(br.readLine() + "\n");
+
+			//Add track method
+			addTrack(br, artistTrack);
+
+			//Loop through arraylist and prints out toString()
+			for(int i = 0; i < artistTrack.size(); i++){
+				outFile.println("Artist: " + artistTrack.get(i).getArtist());
+				outFile.println("# of Track: (" + artistTrack.get(i).getNumTracks() + ")");
+				outFile.println(artistTrack.get(i).toString());
+			}
 			
-			
+			//flush stream. (Write out any buffered output)
+			outFile.flush();
+
 		//Throw error if any
 		}catch(FileNotFoundException e){
 			System.err.println("This file does not exist");
 		}catch(IOException e){
 			System.err.println("There is an IOException");
 		}
-		
+	}
 
 
+	/**
+	**input:** 
+	BufferedReader br,
+	ArrayList of artist
+	**process:** 
+	Read in artist name, trackname, trackposition, and # time streamed. Also checks whether the artist already exist in the arraylist.
+	**output:** 
+	If artist exist, add the track to the object, if NOT creating a new artist and add info. 
+	**/
+	public static void addTrack(BufferedReader br, ArrayList<Artist> artistTrack) throws IOException{
+		//vars
+		Artist artist;
+		String info = "";
+		//while loop that loops through each existing line
+		while((info = br.readLine()) != null){
+			//replace all ", " to a " ". This removes unnessary commas EXCLUDING the comma separting the info. (commas in track name etc).
+			info = info.replaceAll(", ", " ");
+			//replace all quotes in the file. 
+			info = info.replaceAll("\"", "");
+			
+			//Token array
+			String[] tokens = info.split(",");
+			
+			//Find track position
+			int trackPosition = Integer.parseInt(tokens[0]);
+			//Find Song Artist
+			String trackName = tokens[1];
+			//Find track name
+			String artistName = tokens[2];
+			//Find # of time Stream
+			int numStreamed = Integer.parseInt(tokens[3]);
+			//boolean to check if artist exist in the list.
+			boolean exist = true;
+
+			//check if list is empty
+			if(artistTrack.size() == 0){
+				artist = new Artist(artistName, trackPosition, trackName,numStreamed);
+				artistTrack.add(artist);
+			}else{
+				//check if artist exist
+				for(int i = 0; i < artistTrack.size(); i++){
+					if(artistName.equals(artistTrack.get(i).getArtist())){
+						artistTrack.get(i).addTrack(trackPosition, trackName,numStreamed);
+						exist = false;
+					}
+				}
+				//condition if artist does NOT exist
+				if(exist){
+					artist = new Artist(artistName,trackPosition, trackName,numStreamed);
+					artistTrack.add(artist);
+				}
+			}
+		}
 	}
 }
