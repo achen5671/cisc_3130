b786d33e03460a92edb4e54bc70c0ef7c09ba6eb
achen5671 <44120813+achen5671@users.noreply.github.com>
2020-11-08 16:25:30 -0500

    added comments and updated readme file. + added stream counts

--- lab_five/Song.java
+++ lab_five/Song.java
@@ -2,22 +2,30 @@ public class Song{
 	//key
 	private String songTitle; 
 	//instance vars
-	private int streamCount;
+	private String streamCount;
 	private String artistName;
 	private int artistAverageStreamCount;
-
+	//node
 	Song leftChild;
 	Song rightChild;
 
-	public Song(String songTitle, int streamCount, String artistName){
+
+	//constructor
+	public Song(String songTitle, String streamCount, String artistName){
 		this.songTitle = songTitle;
 		this.streamCount = streamCount;
 		this.artistName = artistName;
 
 	}
 
+
+	//getter
 	public String getSongTitle(){
 		return songTitle;
+	}
+	//toString
+	public String toString(){
+		return songTitle + " is by " + artistName + " with a stream count of " + streamCount;
 	}
 
 }
